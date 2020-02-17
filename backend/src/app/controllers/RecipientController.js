7cfa513f70220eccc198d423c1dfed26392c52f8
byronkamal <byronkamal@hotmail.com>
2020-02-17 04:26:49 -0300

    Add recipient controller

---
+++ backend/src/app/controllers/RecipientController.js
@@ -0,0 +1,98 @@
+import * as Yup from 'yup';
+import Recipient from '../models/Recipient';
+import User from '../models/User';
+
+class RecipientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number().required(),
+      complement: Yup.string(),
+      city: Yup.string().required(),
+      state: Yup.string().required(),
+      zip_code: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation has been failed' });
+    }
+
+    const user = await User.findByPk(req.userId);
+    if (!user || user.role !== 0) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const {
+      id,
+      name,
+      street,
+      number,
+      complement,
+      city,
+      state,
+      zip_code,
+    } = await Recipient.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      complement,
+      city,
+      state,
+      zip_code,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number(),
+      complement: Yup.string(),
+      city: Yup.string().required(),
+      state: Yup.string().required(),
+      zip_code: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation has been failed' });
+    }
+
+    const user = await User.findByPk(req.userId);
+    if (!user || user.role !== 0) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const recipient = await Recipient.findByPk(req.params.id);
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Recipient not found' });
+    }
+
+    const {
+      id,
+      name,
+      street,
+      number,
+      complement,
+      city,
+      state,
+      zip_code,
+    } = await recipient.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      complement,
+      city,
+      state,
+      zip_code,
+    });
+  }
+}
+
+export default new RecipientController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,6 +9,9 @@ const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+routes.post('/recipients', RecipientController.store);
+routes.put('/recipients', RecipientController.update);
+
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
