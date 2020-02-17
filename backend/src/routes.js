0f660ed019fc46d508b42235ff8f691479ac895c
byronkamal <byronkamal@hotmail.com>
2020-02-17 02:24:06 -0300

    Creating user controller and using JWT in authentication

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,10 @@
 import express, { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
-
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Iniciando app' });
-});
+// Post
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
